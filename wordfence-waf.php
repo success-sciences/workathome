05ec4d63a29453cc7335214c4b1a5cc3a2db6ec0
Success Sciences <bharris@success-sciences.com>
2020-08-17 18:18:57 -0400

    wp

---
+++ wordfence-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+if (file_exists('C:\\inetpub\\wwwroot\\wp\\wp-content\\plugins\\wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", 'C:\\inetpub\\wwwroot\\wp/wp-content/wflogs/');
+	include_once 'C:\\inetpub\\wwwroot\\wp\\wp-content\\plugins\\wordfence/waf/bootstrap.php';
+}
+?>
