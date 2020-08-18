66adc93378fba3ed03fb374deb09051bce38655c
Success Sciences <bharris@success-sciences.com>
2020-08-18 09:56:57 -0700

    delete

--- wp-content/plugins/official-mailerlite-sign-up-forms/include/templates/admin/header.php
+++
@@ -1,8 +0,0 @@
-<?php defined( 'ABSPATH' ) or die( "No direct access allowed!" ); ?>
-
-<?php if ( $mailerlite_error ): ?>
-    <div class="error">
-        <p><?php echo $mailerlite_error; ?></p>
-    </div>
-
-<?php endif; ?>
